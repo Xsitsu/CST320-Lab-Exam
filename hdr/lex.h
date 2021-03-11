045ca0d874fc404467453591a327e8cf82923a5a
Xsitsu <jacoblocke21@live.com>
2021-03-11 12:27:17 -0800

    Added lex.h

---
+++ hdr/lex.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include "cAstNode.h"
+
+int yyparse();                          // parsing function
+int yylex();                            // scanning function
+extern char *yytext;                    // text of current token
+extern int yylineno;                    // line number of input file
+extern int yynerrs;                     // Num of compile errors
+extern FILE *yyin;                      // FILE pointer for input
+extern cAstNode *yyast_root;            // root of AST
+
