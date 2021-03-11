8e0e0fd8b13c91e5a217b2724fa59b8b9a42604e
Xsitsu <jacoblocke21@live.com>
2021-03-11 12:12:38 -0800

    Filled out cAstNode class.

--- hdr/cAstNode.h
+++ hdr/cAstNode.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <vector>
+
 class cAstNode
 {
+private:
+    std::vector<cAstNode*> m_children;
+
 protected:
+    void AddChild(cAstNode *node);
+    cAstNode* GetChild(int index);
+    int NumChildren();
 
 public:
+    cAstNode();
 
+    virtual void Traverse();
 };
--- src/cAstNode.cpp
+++ src/cAstNode.cpp
@@ -1,2 +1,30 @@
 #include "cAstNode.h"
 
+void cAstNode::AddChild(cAstNode *node)
+{
+    this->m_children.push_back(node);
+}
+
+cAstNode* cAstNode::GetChild(int index)
+{
+    if (index >= (int)this->m_children.size()) return nullptr;
+    return this->m_children[child];
+}
+
+int cAstNode::NumChildren()
+{
+    return (int)this->m_children.size();
+}
+
+cAstNode::cAstNode() : m_children()
+{
+
+}
+
+void cAstNode::Traverse()
+{
+    foreach (auto child : this->m_children)
+    {
+        child->Traverse();
+    }
+}
